ad0a6602b474cf5c754b68ce5cf3c513ad59e5d1
eubrunocoelho <eu.brunocoelho94@gmail.com>
2026-07-03 16:25:44 -0300

    chore: adds dotenv file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { mailRouter } from './mail.routes.ts';
 
-const router = Router();
+const router: Router = Router();
 
 router.use('/mail', mailRouter);
 
--- src/routes/mail.routes.ts
+++ src/routes/mail.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { type Request, type Response, Router } from 'express';
 
-const mailRouter = Router();
+const mailRouter: Router = Router();
 
-mailRouter.get('/', (_req, res) => {
+mailRouter.get('/', (_req: Request, res: Response) => {
 	res.json({
 		message: 'What are you doing here?',
 	});
